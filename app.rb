1be5c5207eca6e86def98c0114d6be379b5a2fbb
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-21 21:48:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-    get '/' do 
-      erb :root 
+    get '/' do
+      erb :root
     end
-    get '/new' do 
+
+    get '/new' do
       erb :'pirates/new'
-  end
-  post '/pirates' do
+    end
+
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
@@ -20,6 +20,6 @@ module FormsLab
 
       erb :'pirates/show'
     end
-end
-
+    
+  end
 end
